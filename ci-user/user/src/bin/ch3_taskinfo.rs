d2e3b888c3b1d9bc6873fa85cea8fca8f18c4ce9
comet959 <1376723538@qq.com>
2022-11-01 19:22:27 +0800

    update, passed all test on 01/11/22 by comet959.

--- ci-user/user/src/bin/ch3_taskinfo.rs
+++ ci-user/user/src/bin/ch3_taskinfo.rs
@@ -23,10 +23,8 @@ pub fn main() -> usize {
     assert_eq!(0, info.syscall_times[SYSCALL_WRITE]); // write调用不执行
     assert!(0 < info.syscall_times[SYSCALL_YIELD]); // yield调用执行至少一次
     assert_eq!(0, info.syscall_times[SYSCALL_EXIT]); // exit调用不执行
-    assert!(t2 - t1 <= info.time + 1); // t2-t1是 sleep(500)的执行时间 info.time应该大于它
-    // println!("t2: {}, t1: {}, t2-t1: {}, info.time+1: {}", t2, t1, t2-t1, info.time+1);
-    // println!("info.time: {}, t3: {}, t1: {}, t3-t1+100:{}", info.time, t3, t1, t3-t1+100);
-    assert!(info.time < t3 - t1 + 100); // t3-t1是sleep(500) + 调用task_info自身的共同时间, 应该比调用自身的时间长
+    assert!(t2 - t1 <= info.time + 1); // t2-t1是 sleep(500)的执行时间 info.time
+    assert!(info.time < t3 - t1 + 100); 
     assert!(info.status == TaskStatus::Running);
 
     // 想想为什么 write 调用是两次
@@ -39,7 +37,6 @@ pub fn main() -> usize {
     assert_eq!(2, info.syscall_times[SYSCALL_WRITE]);
     assert!(0 < info.syscall_times[SYSCALL_YIELD]);
     assert_eq!(0, info.syscall_times[SYSCALL_EXIT]);
-    println!("t4: {}, t1: {}, t4-t1: {}, info.time: {}", t4, t1, t4-t1, info.time);
     assert!(t4 - t1 <= info.time + 1);
     assert!(info.time < t5 - t1 + 100);
     assert!(info.status == TaskStatus::Running);
